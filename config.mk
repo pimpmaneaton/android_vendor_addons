747e5ee0a61e549af05027e5ae512e9adfbca562
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-19 05:59:24 +0800

    misc: Adapt udfps packages for risingOS
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config.mk
+++ config.mk
@@ -285,6 +285,12 @@ PRODUCT_PACKAGES += \
     LineageNavigationBarNoHint \
     LineageBlackTheme
 
+ifeq ($(TARGET_HAS_UDFPS),true)
+PRODUCT_PACKAGES += \
+    UdfpsIcons \
+    UdfpsAnimations
+endif
+
 # Include {Lato,Rubik} fonts
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
